40995846a372a91ba53633c953c4cb8817dedadf
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-25 19:24:12 -0300

    Requisitos 1 e 3 - (2 em andamento)

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const teste = species.find((specie) => specie.name === animal);
+  return teste.residents.every((animalAge) => animalAge > age);
 }
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('otters', 7));
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  } const empregado = employees
+    .filter((person) => person.firstName === employeeName || person.lastName === employeeName);
+    // .filter((person) => employeeName.includes(person.firstName) || employeeName.includes(person.lastName));
+  const personEmployee = empregado[0];
+  return personEmployee;
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName('Emery'));
+console.log(getEmployeeByName('Elser'));
