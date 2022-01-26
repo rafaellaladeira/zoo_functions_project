8b9c79d89ea0ae06c03447ca233f71ef9100ec69
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-26 18:34:37 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const { employees } = data;
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstAnimal = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const findingAnimal = species.find((array) => array.id === firstAnimal).residents;
+  const oldestAnimal = findingAnimal.sort((a, b) => a.age - b.age);
+  const keysFromTheOldest = oldestAnimal[oldestAnimal.length - 1];
+  return Object.values(keysFromTheOldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+console.log(getOldestFromFirstSpecies(stephanieId));
