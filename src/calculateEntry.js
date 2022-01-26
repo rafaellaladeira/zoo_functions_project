c1d7e59d4a85addb8cfce19f7e5d282cc1ac0997
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-01-26 17:22:38 -0300

    Requisitos 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
+const entrants1 = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  const child1 = entrants.filter((filtering) => filtering.age < 18).length;
+  const adult1 = entrants.filter((filtering) => filtering.age >= 18 && filtering.age < 50).length;
+  const senior1 = entrants.filter((filtering) => filtering.age >= 50).length;
+  return {
+    child: child1,
+    adult: adult1,
+    senior: senior1,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const extraindo = countEntrants(entrants);
+  return extraindo.child * prices.child
+  + extraindo.adult * prices.adult
+  + extraindo.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+console.log(countEntrants(entrants1));
+console.log(calculateEntry(entrants1));
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((choose) => choose.name === animal.specie).residents.length;
+  }
+  if (Object.keys(animal).length > 1) {
+    const seachingFemales = species.find((choose) => choose.name === animal.specie).residents;
+    return seachingFemales.filter((female) => female.sex === 'female').length;
+  }
+}
 module.exports = countAnimals;
+
+// console.log(countAnimals());
+// console.log(countAnimals({ specie: 'giraffes' }));
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
